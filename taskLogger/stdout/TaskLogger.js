b7492f131e3466ffbde2f96a717c2c6847d19189
Nazar Rohozhuk <nazar@codefresh.io>
2019-12-19 15:56:39 +0200

    add stdout strategy

--- taskLogger/stdout/TaskLogger.js
+++ taskLogger/stdout/TaskLogger.js
@@ -28,36 +28,36 @@ class StdOutTaskLogger extends TaskLogger {
     }
 
     _reportLastUpdate(value) {
-        this.writter.child('lastUpdate').set(value);
+
     }
 
     reportId() {
-        this.writter.child('id').set(this.jobId);
+
     }
     reportAccountId() {
-        this.writter.child('accountId').set(this.accountId);
+
     }
     _reportMemoryUsage(time, memoryUsage, syncId) {
-        this.writter.child('metrics').child('memory').push({ time, usage: memoryUsage }, syncId);
+
     }
 
     _reportMemoryLimit() {
-        this.writter.child('metrics.limits.memory').push(this.memoryLimit);
+
     }
 
     _reportVisibility() {
-        this.writter.child('visibility').set(this.visibility);
+
     }
 
     _reportData() {
-        this.writter.child('data').set(this.data);
+
     }
 
     _reportStatus() {
-        this.writter.child('status').set(this.status);
+
     }
     _reportLogSize() {
-        this.writter.child('metrics.logs.total').set(this.logSize);
+
     }
 }
 
