b74db29dff9962dbe993dc639a3ad4b252ffeea8
noamdoron <noam@codefresh.com>
2020-01-28 15:04:39 +0200

    fixed hex conversion

--- taskLogger/firebase/step-streams/StepNameTransformStream.js
+++ taskLogger/firebase/step-streams/StepNameTransformStream.js
@@ -7,10 +7,8 @@ class StepNameTransformStream extends Transform {
     }
 
     _transform(chunk, encoding, done) {
-        // const stepNameSizeHeader = new Buffer(1); // Create 1 byte buffer for step name size header // Notice that step name will be limited by 255 chars
         const stepNameSizeHeader = Buffer.alloc(1); // Create 1 byte buffer for step name size header // Notice that step name will be limited by 255 chars
-        // const stepNameLengthHex = '0x' + this.stepName.length.toString(16);
-        const stepNameLengthHex = `0x'${this.stepName.length.toString(16)}`;
+        const stepNameLengthHex = `0x${this.stepName.length.toString(16)}`;
         stepNameSizeHeader.writeUInt8(stepNameLengthHex, 0);
 
         const data = Buffer.concat([stepNameSizeHeader, Buffer.from(this.stepName), chunk]);
