d53f2183c22b14b2e8d6328270dc3e9f7b425854
andrii.shaforostov <andrii@codefresh.io>
2020-03-16 18:38:44 +0200

    fix linter

--- taskLogger/firebase/DebuggerStreamFactory/TransformCutResizeStream.js
+++ taskLogger/firebase/DebuggerStreamFactory/TransformCutResizeStream.js
@@ -3,11 +3,11 @@ const { Transform } = require('stream');
 class TransformCutResizeStream extends Transform {
     _transform(data, encoding, callback) {
         const text = data.toString();
-        if (!!text.match(/^\x1b\[8;\d+;\d+t$/)) {
+        if (text.match(/^\x1b\[8;\d+;\d+t$/)) { // eslint-disable-line no-control-regex
             return callback();
         }
         this.push(text);
-        callback();
+        return callback();
     }
 }
 
